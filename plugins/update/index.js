2b70beaaf55c286cc3e66e726392a28b22f31f83
huan_kong <2564076459@qq.com>
2023-01-12 14:32:52 +0800

    对插件`update`进行了小调整

--- plugins/update/index.js
+++ plugins/update/index.js
@@ -7,50 +7,60 @@ export default async () => {
 //注册事件
 async function event() {
   if (global.config.update.enable) {
+    //手动检查更新
+    RegEvent('message', async (event, context, tags) => {
+      if (context.command) {
+        if (context.command.name === '检查更新') {
+          await checkUpdate(true, context)
+        }
+      }
+    })
+
     await checkUpdate()
     global.config.update.id = setInterval(async () => {
       await checkUpdate()
     }, global.config.update.hz)
   }
-  //手动检查更新
-  RegEvent('message', async (event, context, tags) => {
-    if (context.command) {
-      if (context.command.name === '检查更新') {
-        await checkUpdate(true)
-      }
-    }
-  })
 }
 
 //检查更新
 import fs from 'fs'
 import { compare } from 'compare-versions'
-export const checkUpdate = async (manual = false) => {
+export const checkUpdate = async (manual = false, context) => {
   const { proxy, url } = global.config.update
   try {
     const remote_version = (await fetch(proxy + url)).version
     const local_version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version
     if (compare(local_version, remote_version, '<')) {
       //需要更新，通知admin
-      await sendMsg(
-        global.config.bot.admin,
-        ['kkbot有更新哟~', `最新版本${remote_version}`, `当前版本${local_version}`].join('\n')
-      )
+      const message = [
+        'kkbot有更新哟~',
+        `最新版本${remote_version}`,
+        `当前版本${local_version}`
+      ].join('\n')
+      if (manual) {
+        await replyMsg(context, message)
+      } else {
+        await sendMsg(global.config.bot.admin, message)
+      }
+
       clearInterval(global.config.update.id)
     }
     if (manual && compare(local_version, remote_version, '=')) {
-      await sendMsg(
-        global.config.bot.admin,
+      await replyMsg(
+        context,
         ['kkbot无需更新哟~', `最新版本${remote_version}`, `当前版本${local_version}`].join('\n')
       )
     }
   } catch (error) {
     global.config.update.count++
     if (global.config.update.count === global.config.update.max) {
-      await sendMsg(
-        global.config.bot.admin,
-        ['检查更新失败', `当前版本${local_version} | 请检查您的网络状况！`].join('\n')
-      )
+      const message = ['检查更新失败', `当前版本${local_version} | 请检查您的网络状况！`].join('\n')
+      if (manual) {
+        await replyMsg(context, message)
+      } else {
+        await sendMsg(global.config.bot.admin, message)
+      }
       clearInterval(global.config.bot.id)
     }
   }
