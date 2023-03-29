cc4c20d6f6499a61b793ae34da02bf82cde1c8b7
huan_kong <2564076459@qq.com>
2023-03-29 12:00:50 +0800

    优化逻辑

--- plugins/update/index.js
+++ plugins/update/index.js
@@ -29,9 +29,25 @@ import { compare } from 'compare-versions'
 import { isToday } from '../gugu/index.js'
 export const checkUpdate = async (manual = false, context) => {
   const { proxy, url } = global.config.update
-  const remote_version = (await fetch(proxy + url)).version
+  let remote_version
   const local_version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version
   try {
+    remote_version = (await fetch(proxy + url)).version
+  } catch (error) {
+    console.log(error)
+    global.config.update.count++
+    const message = ['检查更新失败', `当前版本${local_version}`, `请检查您的网络状况！`].join('\n')
+    if (manual) {
+      await replyMsg(context, message)
+    } else {
+      await sendMsg(global.config.bot.admin, message)
+    }
+    if (global.config.update.count === global.config.update.max) {
+      clearInterval(global.config.bot.id)
+    }
+  }
+
+  if (remote_version) {
     if (compare(local_version, remote_version, '<')) {
       //需要更新，通知admin
       const message = [
@@ -57,24 +73,12 @@ export const checkUpdate = async (manual = false, context) => {
         }
       }, 1000 * 60 * 60)
     }
+
     if (manual && compare(local_version, remote_version, '>=')) {
       await replyMsg(
         context,
         ['kkbot无需更新哟~', `最新版本${remote_version}`, `当前版本${local_version}`].join('\n')
       )
-    }
-  } catch (error) {
-    global.config.update.count++
-    if (global.config.update.count === global.config.update.max) {
-      const message = ['检查更新失败', `当前版本${local_version}`, `请检查您的网络状况！`].join(
-        '\n'
-      )
-      if (manual) {
-        await replyMsg(context, message)
-      } else {
-        await sendMsg(global.config.bot.admin, message)
-      }
-      clearInterval(global.config.bot.id)
     }
   }
 }
