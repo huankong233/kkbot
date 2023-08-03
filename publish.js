61b09acd8afa5b55edfa1f4fe90005f5116e1e87
huan_kong <2564076459@qq.com>
2023-08-03 21:25:24 +0800

    :bug: fix: 修复依赖问题

--- publish.js
+++ publish.js
@@ -6,6 +6,7 @@ import fs from 'fs'
 
 const pluginDirs = ['plugins_dependencies', 'plugins']
 const originPackages = {
+  dayjs: '^1.11.9',
   'node-fetch': '^3.3.1',
   qs: '^6.11.2',
   'abort-controller': '^3.0.0',
